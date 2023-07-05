2bbcb9fb00404dd1e77d37f4e82e5e3bb059415e
Abdulmelik619 <abdulmelikambw619@gmail.com>
2023-07-05 23:14:58 +0200

    making executable

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,25 +1,27 @@
 #!/usr/bin/env bash
+# script that sets up web servers for the deployment of web_static
+sudo apt-get update
+sudo apt-get -y install nginx
+sudo ufw allow 'Nginx HTTP'
 
-# Install Nginx if it is not already installed
-if ! dpkg -s nginx > /dev/null 2>&1; then
-    sudo apt-get -y update
-    sudo apt-get -y install nginx
-fi
+sudo mkdir -p /data/
+sudo mkdir -p /data/web_static/
+sudo mkdir -p /data/web_static/releases/
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+sudo touch /data/web_static/releases/test/index.html
+sudo echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" | sudo tee /data/web_static/releases/test/index.html
 
-# Create the necessary directories
-sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
+sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
 
-# Create a fake HTML file for testing
-echo "<html><head></head><body>My Name is Abdulmelik Ambaw</body></html>" | sudo tee /data/web_static/releases/test/index.html
-
-# Create a symbolic link to the /data/web_static/releases/test/ folder
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-
-# Set ownership of /data/ to the ubuntu user and group
 sudo chown -R ubuntu:ubuntu /data/
 
-# Update the nginx configuration to serve the content of /data/web_static/current/ to hbnb_static
-sudo sed -i '/listen 80 default_server;/a location /hbnb_static/ {\n\talias /data/web_static/current/;\n}\n' /etc/nginx/sites-available/default
+sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
 
-# Restart Nginx to apply the changes
 sudo service nginx restart
